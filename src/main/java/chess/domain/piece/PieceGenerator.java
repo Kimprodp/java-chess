0355bfe2bc4eaa04666c99d6771be92e2a4ebe82
KIMGYUTAE <kiimwf0819@gmail.com>
2024-03-25 14:14:25 +0900

    refactor(PieceGenerator): 클래스명  변경 및 리팩터링
    - 클래스명 변경 : ChessPieceGenerator -> PieceGenerator)

---
+++ src/main/java/chess/domain/piece/PieceGenerator.java
@@ -0,0 +1,24 @@
+package chess.domain.piece;
+
+import chess.domain.position.Position;
+
+public class PieceGenerator {
+
+    private static final PieceGenerator INSTANCE = new PieceGenerator();
+
+    private final PieceCampDeterminer pieceCampDeterminer = PieceCampDeterminer.getInstance();
+    private final PieceTypeDeterminer pieceTypeDeterminer = PieceTypeDeterminer.getInstance();
+
+    private PieceGenerator() {
+    }
+
+    public static PieceGenerator getInstance() {
+        return INSTANCE;
+    }
+
+    public Piece generate(Position position) {
+        ChessPieceType pieceType = pieceTypeDeterminer.determine(position);
+        Camp camp = pieceCampDeterminer.determineCamp(position.getNumbering());
+        return new Piece(pieceType, camp);
+    }
+}
