193666211f2eeeb9b3ab797391b1e3ef387de610
KIMGYUTAE <kiimwf0819@gmail.com>
2024-03-27 16:42:38 +0900

    refactor(PieceType): 체스 타입별 점수 계산 기능 추가

--- src/main/java/chess/domain/piece/PieceType.java
+++ src/main/java/chess/domain/piece/PieceType.java
@@ -8,27 +8,94 @@ import chess.domain.piece.movestrategy.MoveStrategy;
 import chess.domain.piece.movestrategy.PawnMoveStrategy;
 import chess.domain.piece.movestrategy.QueenMoveStrategy;
 import chess.domain.piece.movestrategy.RookMoveStrategy;
+import chess.domain.position.Lettering;
 import chess.domain.position.Position;
+import java.util.List;
 import java.util.Set;
 import java.util.function.Supplier;
 
 public enum PieceType {
 
-    KING(KingMoveStrategy::getInstance),
-    QUEEN(QueenMoveStrategy::getInstance),
-    BISHOP(BishopMoveStrategy::getInstance),
-    KNIGHT(KnightMoveStrategy::getInstance),
-    ROOK(RookMoveStrategy::getInstance),
-    PAWN(PawnMoveStrategy::getInstance);
+    KING(KingMoveStrategy::getInstance, 0.0),
+    QUEEN(QueenMoveStrategy::getInstance, 9.0),
+    BISHOP(BishopMoveStrategy::getInstance, 3.0),
+    KNIGHT(KnightMoveStrategy::getInstance, 2.5),
+    ROOK(RookMoveStrategy::getInstance, 5.0),
+    PAWN(PawnMoveStrategy::getInstance, 1.0);
+
+    private static final double DEFAULT_SCORE = 0.0;
+    private static final double VERTICAL_DUPLICATE_PAWN_SCORE = 0.5;
+    private static final int DUPLICATE_SCORE_CRITERIA = 1;
 
     private final Supplier<MoveStrategy> moveStrategySupplier;
+    private final double score;
 
-    PieceType(Supplier<MoveStrategy> moveStrategySupplier) {
+    PieceType(Supplier<MoveStrategy> moveStrategySupplier, double score) {
         this.moveStrategySupplier = moveStrategySupplier;
+        this.score = score;
+    }
+
+    public static double calculatePawnScore(List<Piece> pawnPieces, PiecePosition piecePosition) {
+        validateEmpty(pawnPieces);
+        validatePieceIsPawn(pawnPieces);
+        validateSameCamp(pawnPieces);
+        return calculateVerticalPawnScore(pawnPieces, piecePosition);
+    }
+
+    private static double calculateVerticalPawnScore(List<Piece> pawnPieces, PiecePosition piecePosition) {
+        double pawnScore = DEFAULT_SCORE;
+        for (Lettering value : Lettering.values()) {
+            int pawnCountInLettering = (int) pawnPieces.stream()
+                    .filter(piece -> piecePosition.findPositionByPiece(piece).getLettering() == value)
+                    .count();
+
+            pawnScore += sumPawnScore(pawnCountInLettering);
+        }
+
+        return pawnScore;
+    }
+
+    private static double sumPawnScore(int pawnCountInLettering) {
+        if (pawnCountInLettering < DUPLICATE_SCORE_CRITERIA) {
+            return DEFAULT_SCORE;
+        }
+        if (pawnCountInLettering == DUPLICATE_SCORE_CRITERIA) {
+            return PAWN.score;
+        }
+        return (double) pawnCountInLettering * VERTICAL_DUPLICATE_PAWN_SCORE;
+    }
+
+    private static void validateEmpty(List<Piece> pawnPieces) {
+        if (pawnPieces.isEmpty()) {
+            throw new IllegalArgumentException("[ERROR] 폰 점수를 계산하기 위한 체스말이 존재하지 않습니다.");
+        }
+    }
+
+    private static void validatePieceIsPawn(List<Piece> pawnPieces) {
+        boolean isAllPawn = pawnPieces.stream()
+                .allMatch(piece -> piece.getPieceType() == PAWN);
+
+        if (!isAllPawn) {
+            throw new IllegalArgumentException("[ERROR] 폰 점수 계산은 폰 타입의 체스말만 가능합니다.");
+        }
+    }
+
+    private static void validateSameCamp(List<Piece> pawnPieces) {
+        Piece standardPiece = pawnPieces.get(0);
+        boolean isAllSameCamp = pawnPieces.stream()
+                .allMatch(piece -> piece.getCamp() == standardPiece.getCamp());
+
+        if (!isAllSameCamp) {
+            throw new IllegalArgumentException("[ERROR] 폰 점수 계산은 같은 진영끼리의 체스말만 가능합니다.");
+        }
     }
 
     public Set<Position> executeMoveStrategy(Position standardPosition, PiecePosition piecePosition) {
         MoveStrategy moveStrategy = moveStrategySupplier.get();
         return moveStrategy.move(standardPosition, piecePosition);
+    }
+
+    public double getScore() {
+        return score;
     }
 }
--- src/test/java/chess/domain/piece/PieceTypeTest.java
+++ src/test/java/chess/domain/piece/PieceTypeTest.java
@@ -3,12 +3,16 @@ package chess.domain.piece;
 import static chess.domain.TestSetting.D2;
 import static chess.domain.TestSetting.D3;
 import static chess.domain.TestSetting.D4;
+import static chess.domain.TestSetting.KNIGHT_WHITE;
+import static chess.domain.TestSetting.PAWN_BLACK;
 import static chess.domain.TestSetting.PAWN_WHITE;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import chess.domain.PiecePosition;
 import chess.domain.position.Position;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import org.junit.jupiter.api.DisplayNameGeneration;
@@ -29,6 +33,73 @@ class PieceTypeTest {
         Set<Position> pawnMovableRange = PieceType.PAWN.executeMoveStrategy(D2, piecePosition);
 
         //then
-        assertThat(pawnMovableRange).containsExactly(D3, D4);
+        assertThat(pawnMovableRange).containsExactlyInAnyOrder(D3, D4);
+    }
+
+    @Test
+    void 폰의_기본_점수는_1점으로_계산() {
+        //given
+        Map<Position, Piece> testPosition = new HashMap<>();
+        testPosition.put(D2, PAWN_WHITE);
+        PiecePosition piecePosition = new PiecePosition(testPosition);
+
+        //when
+        double pawnScore = PieceType.calculatePawnScore(List.of(PAWN_WHITE), piecePosition);
+
+        //then
+        assertThat(pawnScore).isEqualTo(1);
+    }
+
+    @Test
+    void 폰은_같은_세로줄에_같은_색의_폰이_있는_경우_1점이_아닌_05점으로_계산() {
+        //given
+        Map<Position, Piece> testPosition = new HashMap<>();
+        testPosition.put(D2, PAWN_WHITE);
+        testPosition.put(D3, PAWN_WHITE);
+        testPosition.put(D4, PAWN_WHITE);
+        PiecePosition piecePosition = new PiecePosition(testPosition);
+        List<Piece> pawnPieces = piecePosition.findPieceByTypeAndCamp(PieceType.PAWN, Camp.WHITE);
+
+        //when
+        double pawnScore = PieceType.calculatePawnScore(pawnPieces, piecePosition);
+
+        //then
+        assertThat(pawnScore).isEqualTo(1.5);
+    }
+
+    @Test
+    void 폰_점수_계산시_체스말이_없을_경우_예외발생() {
+        //given
+        Map<Position, Piece> testPosition = new HashMap<>();
+        PiecePosition piecePosition = new PiecePosition(testPosition);
+
+        //when, then
+        assertThatThrownBy(() -> PieceType.calculatePawnScore(List.of(), piecePosition))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 폰_점수_계산시_체스말이_폰이_아닐_경우_예외발생() {
+        //given
+        Map<Position, Piece> testPosition = new HashMap<>();
+        testPosition.put(D2, KNIGHT_WHITE);
+        PiecePosition piecePosition = new PiecePosition(testPosition);
+
+        //when, then
+        assertThatThrownBy(() -> PieceType.calculatePawnScore(List.of(KNIGHT_WHITE), piecePosition))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 폰_점수_계산시_진영이_다를_경우_예외발생() {
+        //given
+        Map<Position, Piece> testPosition = new HashMap<>();
+        testPosition.put(D2, PAWN_WHITE);
+        testPosition.put(D3, PAWN_BLACK);
+        PiecePosition piecePosition = new PiecePosition(testPosition);
+
+        //when, then
+        assertThatThrownBy(() -> PieceType.calculatePawnScore(List.of(PAWN_WHITE, PAWN_BLACK), piecePosition))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
