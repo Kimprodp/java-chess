e8f4c71ad31ea8bb04213359bca4d026d82fa274
KIMGYUTAE <kiimwf0819@gmail.com>
2024-03-28 11:12:10 +0900

    feat(TurnExecutor): 턴 실행관리 도메인 추가 및 테스트 작성

---
+++ src/main/java/chess/domain/TurnExecutor.java
@@ -0,0 +1,47 @@
+package chess.domain;
+
+import chess.domain.piece.Camp;
+import chess.domain.piece.Piece;
+import chess.domain.position.Position;
+import chess.dto.PiecePositionDto;
+
+public class TurnExecutor {
+
+    private final PiecePosition piecePosition;
+    private Camp turnToMove;
+
+    public TurnExecutor(PiecePosition piecePosition) {
+        this.piecePosition = piecePosition;
+        this.turnToMove = Camp.WHITE;
+    }
+
+    public PiecePositionDto execute(Position moveSource, Position target, ChessStatus chessStatus) {
+        validatePieceExistsOnSquare(moveSource);
+        Piece pieceToMove = piecePosition.findChessPieceOnPosition(moveSource);
+        validateTurnSide(pieceToMove);
+        pieceToMove.move(moveSource, target, piecePosition);
+        chessStatus.updateStatus(piecePosition);
+        changeTurn();
+        return piecePosition.createDto();
+    }
+
+    private void changeTurn() {
+        if (turnToMove == Camp.WHITE) {
+            turnToMove = Camp.BLACK;
+            return;
+        }
+        turnToMove = Camp.WHITE;
+    }
+
+    private void validatePieceExistsOnSquare(Position moveSource) {
+        if (!piecePosition.hasPieceAt(moveSource)) {
+            throw new IllegalArgumentException("[ERROR] 이동할 체스말이 없습니다. : " + moveSource);
+        }
+    }
+
+    private void validateTurnSide(Piece pieceToMove) {
+        if (pieceToMove.getCamp() != turnToMove) {
+            throw new IllegalArgumentException("[ERROR] 현재 실행 가능한 턴의 체스말이 아닙니다. : " + pieceToMove);
+        }
+    }
+}
