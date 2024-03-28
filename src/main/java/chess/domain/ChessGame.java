956ec662c743827aa85a51381ca1c6970a03e2d8
KIMGYUTAE <kiimwf0819@gmail.com>
2024-03-28 11:22:59 +0900

    feat(ChessGame): 체스 게임 도메인 추가 및 테스트 작성

--- src/main/java/chess/domain/ChessGame.java
+++ src/main/java/chess/domain/ChessGame.java
@@ -1,27 +1,35 @@
 package chess.domain;
 
-import chess.domain.chessboard.ChessBoard;
-import chess.domain.chessboard.Square;
-import chess.domain.chesspiece.ChessPiece;
+import chess.domain.position.Position;
+import chess.dto.ChessStatusDto;
+import chess.dto.PiecePositionDto;
 
 public class ChessGame {
 
-    private final ChessBoard chessBoard;
+    private final TurnExecutor turnExecutor;
+    private final ChessStatus chessStatus;
 
-    public ChessGame(ChessBoard chessBoard) {
-        this.chessBoard = chessBoard;
+    public ChessGame(TurnExecutor turnExecutor, ChessStatus chessStatus) {
+        this.turnExecutor = turnExecutor;
+        this.chessStatus = chessStatus;
     }
 
-    public void executeTurn(Square moveSource, Square target) {
-        ChessPiece chessPieceOnSquare = chessBoard.findChessPieceOnSquare(moveSource).orElse(null);
-        validateEmptyChessPiece(chessPieceOnSquare);
-
-        chessPieceOnSquare.move(chessBoard, moveSource, target);
+    public PiecePositionDto executeTurn(Position moveSource, Position target) {
+        validateGameInProgress();
+        return turnExecutor.execute(moveSource, target, chessStatus);
     }
 
-    private void validateEmptyChessPiece(ChessPiece chessPieceOnSquare) {
-        if (chessPieceOnSquare == null) {
-            throw new IllegalArgumentException("[ERROR] 이동할 체스말이 없습니다.");
+    public ChessStatusDto requestStatus() {
+        return chessStatus.getStatus();
+    }
+
+    public boolean isGameInProgress() {
+        return chessStatus.isGameInProgress();
+    }
+
+    private void validateGameInProgress() {
+        if (!chessStatus.isGameInProgress()) {
+            throw new IllegalArgumentException("[ERROR] 종료된 게임입니다.");
         }
     }
 }
